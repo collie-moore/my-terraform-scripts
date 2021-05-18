81c9a9920d7e34e3b3786a50789756d795bf755d
Amdava Collins <amc1@students.uonbi.ac.ke>
2021-05-18 13:22:06 +0300

    first commit

---
+++ terraform-gke-master/variables.tf
@@ -0,0 +1,20 @@
+variable "name" {
+  default = "mycluster"
+}
+
+variable "project" {
+  default = "banded-nuance-310605"
+}
+
+variable "location" {
+  default = "us-central1"
+}
+
+variable "initial_node_count" {
+  default = 3
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
+
